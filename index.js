a009d1522d6e02f72d64dfb22be14f17c81c6504
Maedea9 <melaniecascante.j@gmail.com>
2023-04-17 19:50:20 -0500

    add linters and fix errors

--- index.js
+++ index.js
@@ -4,11 +4,43 @@
 //   renderTasks, addTask,
 // } from './modules/functionality.js';
 
-//ADD NEW TASK
+// RENDER TASKS
+const taskList = document.getElementById('taskList');
+
+const renderTasks = () => {
+  const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
+  taskList.innerHTML = '';
+  // Loop through tasks and create taskBox element for each task
+  toDoArray.forEach((task) => {
+    const taskBox = document.createElement('div');
+    taskBox.classList = 'task-box-css';
+    let taskCompleted = '';
+    if (task.completed) taskCompleted = 'checked';
+    taskBox.innerHTML = ` <div class="task-activity">
+                      <input type="checkbox" class="input-check" ${taskCompleted}>
+                      <input type="text" class="text-input" value="${task.description}">
+                    </div>
+                    <i class="trash-icon" id="delete-icon">&#x1F5D1;</i>
+                    <i class="move-icon" id="order-icon">&#x22EE;</i>`;
+    taskList.appendChild(taskBox);
+
+    // COMPLETED TASKS//
+    const checkbox = taskBox.querySelector('.input-check');
+    checkbox.addEventListener('click', (e) => {
+      const taskBox = e.target.parentElement.parentElement;
+      const taskIndex = Array.from(taskBox.parentElement.children).indexOf(taskBox);
+      const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
+      toDoArray[taskIndex].completed = e.target.checked;
+      localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
+    });
+  });
+};
+renderTasks();
+
+// ADD NEW TASK
 
 const newTask = document.querySelector('.new-task');
 const addTask = () => {
-  
   if (newTask.value !== '') {
     const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
     toDoArray.push({ completed: false, description: newTask.value, id: Date.now() });
@@ -17,9 +49,9 @@ const addTask = () => {
     }
     localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
     newTask.value = '';
-    // renderTasks(); // 
+    // renderTasks(); //
   }
-  renderTasks(); // 
+  renderTasks(); //
   // newTask.value = '';
 };
 
@@ -29,7 +61,7 @@ addButton.addEventListener('click', () => {
   // renderTasks();
 });
 
-const taskInput = document.getElementById("taskInput");
+const taskInput = document.getElementById('taskInput');
 taskInput.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     e.preventDefault(); // prevents page to reload when enter is pressed
@@ -38,96 +70,64 @@ taskInput.addEventListener('keypress', (e) => {
   }
 });
 
-//RENDER TASKS
+// DELETE TASKS//
 
-const renderTasks = () => {
-    const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
-    taskList.innerHTML = '';
-    // Loop through tasks and create taskBox element for each task
-    toDoArray.forEach((task) => {
-      const taskBox = document.createElement('div');
-      taskBox.classList = 'task-box-css';
-      let taskCompleted = '';
-      if (task.completed) taskCompleted = 'checked';
-      taskBox.innerHTML = ` <div class="task-activity">
-                      <input type="checkbox" class="input-check" ${taskCompleted}>
-                      <input type="text" class="text-input" value="${task.description}">
-                    </div>
-                    <i class="trash-icon" id="delete-icon">&#x1F5D1;</i>
-                    <i class="move-icon" id="order-icon">&#x22EE;</i>`;
-      taskList.appendChild(taskBox);
-  
-      // COMPLETED TASKS//
-      const checkbox = taskBox.querySelector('.input-check');
-      checkbox.addEventListener('click', (e) => {
-        const taskBox = e.target.parentElement.parentElement;
-        const taskIndex = Array.from(taskBox.parentElement.children).indexOf(taskBox);
-        const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
-        toDoArray[taskIndex].completed = e.target.checked;
-        localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
-      });
-    });
-  };
-  renderTasks();
-
-  //DELETE TASKS//
-
-  const deleteTask = (index) => {
-    const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
-    toDoArray.splice(index, 1);
-    for (let i = 0; i < toDoArray.length; i++) {
-      toDoArray[i].index = i;
-    }
-    localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
+const deleteTask = (index) => {
+  const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
+  toDoArray.splice(index, 1);
+  for (let i = 0; i < toDoArray.length; i += 1) {
+    toDoArray[i].index = i;
   }
-  
-  const listContainer = document.querySelector('.list-content');
-  
-  listContainer.addEventListener('click', (event) => {
-    const removeTask = event.target.closest('.trash-icon');
-    if (removeTask) {
-      const trashIcons = listContainer.querySelectorAll('.trash-icon');
-      const index = Array.from(trashIcons).indexOf(removeTask);
-      deleteTask(index);
-      renderTasks();
-    }
-  });
-  
-  ///EDIT TASKS //////
+  localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
+};
 
-  const editTask= (index) => {
-    const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
-    const textInputs= document.querySelectorAll('.text-input');
-    textInputs[index].addEventListener('change', () => {
-      toDoArray[index].description = textInputs[index].value;
-      localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
-    });
-    };
-  
-  //add event listener for editing a task//
-  listContainer.addEventListener('click', (event) => {
-    const textInput = event.target.closest('.text-input');
-    if (textInput) {
-      const textInputs = listContainer.querySelectorAll('.text-input');
-      const index = Array.from(textInputs).indexOf(textInput);
-      editTask(index);
-    }
+const listContainer = document.querySelector('.list-content');
+
+listContainer.addEventListener('click', (event) => {
+  const removeTask = event.target.closest('.trash-icon');
+  if (removeTask) {
+    const trashIcons = listContainer.querySelectorAll('.trash-icon');
+    const index = Array.from(trashIcons).indexOf(removeTask);
+    deleteTask(index);
+    renderTasks();
+  }
+});
+
+/// EDIT TASKS //////
+
+const editTask = (index) => {
+  const toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
+  const textInputs = document.querySelectorAll('.text-input');
+  textInputs[index].addEventListener('change', () => {
+    toDoArray[index].description = textInputs[index].value;
+    localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
   });
+};
+
+// add event listener for editing a task//
+listContainer.addEventListener('click', (event) => {
+  const textInput = event.target.closest('.text-input');
+  if (textInput) {
+    const textInputs = listContainer.querySelectorAll('.text-input');
+    const index = Array.from(textInputs).indexOf(textInput);
+    editTask(index);
+  }
+});
 
 // // CLEAR ALL COMPLETED//
 
 const clearAllCompleted = () => {
-    let toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
-    toDoArray = toDoArray.filter((task) => !task.completed);
-    toDoArray = toDoArray.map((task, index) => {
-      task.index = index + 1;
-      return task;
-    });
-    localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
-  };
-  
-  const clearButton = document.getElementById('clear-complete');
-  clearButton.addEventListener('click', () => {
+  let toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
+  toDoArray = toDoArray.filter((task) => !task.completed);
+  toDoArray = toDoArray.map((task, index) => {
+    task.index = index + 1;
+    return task;
+  });
+  localStorage.setItem('toDoArray', JSON.stringify(toDoArray));
+};
+
+const clearButton = document.getElementById('clear-complete');
+clearButton.addEventListener('click', () => {
   clearAllCompleted();
   renderTasks();
 });
