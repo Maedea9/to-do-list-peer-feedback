d43b6a4588341ccf63fb72409b7349cdf77d05ed
Maedea9 <melaniecascante.j@gmail.com>
2023-04-17 20:05:09 -0500

    Added webpack config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import './style.css';
+import './styles.css';
 
 // import {
 //   renderTasks, addTask,
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+	mode: 'development',
+	devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+			{
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
